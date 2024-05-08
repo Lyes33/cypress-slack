633e3b50146f4a74a63fa915d28486e939307026
lyes <bob.lyes@live.com>
2024-05-08 13:06:46 +0200

    fix test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,6 +4,6 @@ describe('Test suite login', () => {
     cy.get('[data-test="username"]').type('standard_user')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('[data-test="login-button"]').click()
-    cy.url().should('contain', 'inventoryyy')
+    cy.url().should('contain', 'inventory')
   })
 })
